332460fbc95109dfa00304a213ca0319a56664a0
Karen Goytizolo <shakitalinda20@hotmail.com>
2019-10-06 19:45:04 -0500

    Enhancing User Forms
    Enhancing User Forms in React JS

--- app/components/User.js
+++ app/components/User.js
@@ -9,29 +9,25 @@ class User extends Component{
         this.state = {
             userMessage: "",
             action: props.action,
-            regFirstName: "",
-            regLastName: "",
-            regEmail: "",
-            regPassword: "",
-            regPassword2: "",
-            regSex: "",
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            password2: "",
+            sex: "M",
             regCategoriesSelected: [],
-            value: "0"
+            age: "0",
+            aboutUser: "Put something about yourself"
         }
         this.handleSubmit = this.handleSubmit.bind(this);               //Binding is important in order to set any state object from an event
         this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleChange = this.handleChange.bind(this); 
         console.log("Action:" + props.action)        
     }
 
     //************** User's events ****************************/
-    handleInputChange(event){                   //For input forms only - updates the state key corresponding to the given input name
-        const {name, value} = event.target;     //Similar to: [event.target.name]: event.target.value   (inside this.setState)
-        this.setState({[name]: value})          //In Js, [] represents string values (ES6 computed property name syntax)
-    }
-
-    handleChange(event){
-        this.setState({value: event.target.value});     //For other elements: gets the value from the form target          
+    handleInputChange(event){                                   //For input forms only - updates the state key corresponding to the given input name
+        const {name, value, type, checked} = event.target;                                          //Event properties ---> Similar to: [event.target.name]: event.target.value   (inside this.setState)
+        type === "checkbox" ? this.setState({[name]: checked}) : this.setState({[name]: value})     //In Js, [] represents string values (ES6 computed property name syntax)
     }
     
     handleSubmit(event){
@@ -47,11 +43,13 @@ class User extends Component{
             )
         }
         
-        console.log("New user's name (Reg): " + this.state.regFirstName);
-        console.log("New user's lastname (Reg): " + this.state.regLastName);
-        console.log("New user's email (Reg): " + this.state.regEmail);
-        console.log("New user's password (Reg): " + this.state.regPassword);    
-        console.log("New user's age (Reg): " + this.state.value);                    
+        console.log("New user's name (Reg): " + this.state.firstName);
+        console.log("New user's lastname (Reg): " + this.state.lastName);
+        console.log("New user's email (Reg): " + this.state.email);
+        console.log("New user's password (Reg): " + this.state.password);    
+        console.log("New user's age range (Reg): " + this.state.age); 
+        console.log("New user's gender: (Reg): " + this.state.sex);
+        console.log("About the user: (Reg): " + this.state.aboutUser);                
     }
 
     render(){
@@ -72,32 +70,32 @@ class User extends Component{
                         <tr>
                             <td>First Name:&nbsp;</td>        
                             <td><input type="text" 
-                                        value={this.state.regFirstName} 
-                                        name="regFirstName" 
+                                        value={this.state.firstName} 
+                                        name="firstName" 
                                         placeholder="Enter First Name" 
                                         onChange={this.handleInputChange} /></td>                                
                         </tr>
                         <tr>
                             <td>Last Name:&nbsp;</td>        
                             <td><input type="text"
-                                        value={this.state.regLastName}
-                                        name="regLastName" 
+                                        value={this.state.lastName}
+                                        name="lastName" 
                                         placeholder="Enter Last Name"
                                         onChange={this.handleInputChange} /></td>                                
                         </tr>                    
                         <tr>
                             <td>Email:&nbsp;</td>        
                             <td><input type="text"
-                                        value={this.state.regEmail}
-                                        name="regEmail"
+                                        value={this.state.email}
+                                        name="email"
                                         placeholder="Enter your Email"
                                         onChange={this.handleInputChange} /></td>                                
                         </tr>                                       
                         <tr>
                             <td>Password:&nbsp;</td>        
                             <td><input type='password'
-                                        value={this.state.regPassword}
-                                        name="regPassword"
+                                        value={this.state.password}
+                                        name="password"
                                         placeholder="Enter a Password" 
                                         onChange={this.handleInputChange}
                                         /></td>                                
@@ -105,8 +103,8 @@ class User extends Component{
                         <tr>
                             <td>Repeat Password:&nbsp;</td>        
                             <td><input type='password'
-                                        value={this.state.regPassword2}
-                                        name="regPassword2"
+                                        value={this.state.password2}
+                                        name="password2"
                                         placeholder="Enter same Password Again" 
                                         onChange={this.handleInputChange}
                                         /></td>                                
@@ -114,7 +112,11 @@ class User extends Component{
                         <tr>
                             <td>Age:&nbsp;</td>
                             <td>
-                                <select value={this.state.value} onChange={this.handleChange}>
+                                <select 
+                                        value={this.state.age} 
+                                        onChange={this.handleInputChange}
+                                        name="age"
+                                    >
                                     <option value="0">Please, select your age</option>
                                     <option value="1">Under 21</option>
                                     <option value="2">Between 21 - 35</option>
@@ -125,9 +127,24 @@ class User extends Component{
                             </td>
                         </tr>                                                                                
                         <tr>
-                            <td><p>Sex:&nbsp;</p></td>
-                            <td><input type='radio' name='gender' value='M' />Male&nbsp;<input type='radio' name='gender' value = 'F' />Female</td>
+                            <td><p>Gender:&nbsp;</p></td>
+                            <td>
+                                <input type='radio' name='sex' value='M' checked={this.state.sex === 'M'} onChange={this.handleInputChange} />Male&nbsp;
+                                <input type='radio' name='sex' value='F' checked={this.state.sex === 'F'} onChange={this.handleInputChange} />Female
+                            </td>
                         </tr>
+                        <tr>
+                            <td colSpan='2'><p><b>Tell us a bit more about yourself:&nbsp;</b></p></td>
+                        </tr>                        
+                        <tr>
+                            <td colSpan='2'>
+                                <textarea 
+                                    name="aboutUser"
+                                    value={this.state.aboutUser}
+                                    onChange={this.handleInputChange} 
+                                />
+                            </td>
+                        </tr>                                                
                         <tr>
                             <td colSpan='2'><p><b>Which products are you interested in?:&nbsp;</b></p></td>
                         </tr>
